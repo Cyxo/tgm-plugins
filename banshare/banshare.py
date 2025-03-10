8d50fbdf2f962ceec761ffcf3222b048bd14df14
Cyxo <8999264+Cyxo@users.noreply.github.com>
2025-03-10 01:11:29 +0100

    Add reason to banshares

--- banshare/banshare.py
+++ banshare/banshare.py
@@ -13,28 +13,29 @@ class Banshare(commands.Cog):
         self.bot = bot
         self.channel = self.bot.get_channel(CHANNEL_ID)
 
-        self.banshared = set()
+        self.banshared = {}
         if path.exists("banshared.json"):
             with open("banshared.json", "r") as f:
-                self.banshared = set(json.load(f))
+                self.banshared = json.load(f)
 
     @commands.Cog.listener("on_member_join")
     async def welcome_on_member_join(self, member: discord.Member):
         if self.channel is not None:
-            if member.id in self.banshared:
-                await self.channel.send(f"<@&1292946300865876142> The new member <@{member.id}> is in a banshare from partnered servers!")
+            if str(member.id) in self.banshared.keys():
+                await self.channel.send(f"<@&1292946300865876142> The new member <@{member.id}> is in a banshare from partnered servers!\n"
+                                        "Here was the reason: \"{self.banshared[str(member.id)]}\"")
 
     @commands.command()
-    async def banshare(self, ctx: commands.Context, id: int):
+    async def banshare(self, ctx: commands.Context, id: int, *, reason: str):
         m = ctx.guild.get_member(id)
         if m is not None:
             await self.channel.send(":warning: The member is already in the server!")
         
-        self.banshared.add(id)
+        self.banshared[str(id)] = reason
         j = json.dumps(list(self.banshared))
         with open("banshared.json", "w") as f:
             f.write(j)
-        await self.channel.send(f"<@{id}> added to the banshare list!")
+        await self.channel.send(f"<@{id}> added to the banshare list! Reason: \"{reason}\"")
 
 
 async def setup(bot):
